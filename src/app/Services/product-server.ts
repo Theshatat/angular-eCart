588082fce6269349661125d26c6a79eb0b319bf7
AhmedShatat <ahmedshatat025@gmail.com>
2026-06-12 19:08:11 +0300

    Base URL

--- src/app/Services/product-server.ts
+++ src/app/Services/product-server.ts
@@ -2,12 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IServerProduct } from '../Models/iserver-product';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../Environment/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductServer {
-   apiUrl = 'http://localhost:3000/products';
+   apiUrl = `${environment.apiUrl}/products`;
 
    // Inject HttpClient to make HTTP requests
   constructor(private http: HttpClient) {}
